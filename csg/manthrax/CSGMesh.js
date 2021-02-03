d2d1e6d1685ed0377a2fbf6569a453267119c464
Lee-hoseong <69020865+Lee-hoseong@users.noreply.github.com>
2021-02-03 14:40:33 +0900

    Update CSGMesh.js

--- csg/manthrax/CSGMesh.js
+++ csg/manthrax/CSGMesh.js
@@ -93,7 +93,7 @@ CSG.fromPolygons=function(polygons) {
 //     new CSG.Vector([1, 2, 3]);
 //     new CSG.Vector({ x: 1, y: 2, z: 3 });
 
-class Vector extends new THREE.Vector3 {
+class Vector extends Vector3 {
     constructor(x, y, z) {
         if (arguments.length == 3)
             super(x, y, z)
